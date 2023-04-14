956343120b0da43c7e0f0e8e08768ef7b354625d
BriaLatt <112596743+BriaLatt@users.noreply.github.com>
2023-04-14 02:32:30 +0000

    "welp"

--- 02_05b/index.php
+++ 02_05b/index.php
@@ -7,6 +7,22 @@ function print_array($a)
 	echo '</pre>';
 }
 
+function get_users()
+{
+	return file_get_contents('visits.txt');
+}
+
+function log_visit()
+{
+	$visits = (int)file_get_contents('visits.txt');
+	$visits++;
+	file_put_contents('visits.txt', $visits);
+}
+
+if (!isset($_COOKIE['visited'])) {
+	setcookie('visited', true, time() + (86400 * 30));
+	log_visit();
+}
 
 function sanitize_form()
 {
@@ -14,16 +30,27 @@ function sanitize_form()
 	foreach ($_POST as $name => $value) {
 		switch ($name) {
 			case 'email':
+				echo 'in email';
 				$value = filter_var($value, FILTER_SANITIZE_EMAIL);
 				break;
 			case 'message':
+				echo 'in email';
 				$value = filter_var(htmlspecialchars($value), FILTER_SANITIZE_ADD_SLASHES);
+				break;
+			case 'phoneNumber':
+				$value = preg_replace('/\D/', '', $value);
+				if (strlen($value) != 10) {
+					$value = '';
+				} else {
+					$value = '(' . substr($value, 0, 3) . ') ' . substr($value, 3, 3) . '-' . substr($value, 6); // Format to (xxx) xxx-xxxx
+				}
 				break;
 			default:
 				$value = filter_var(preg_replace('/[^A-Za-z0-9 \-]/', '', $value), FILTER_SANITIZE_ADD_SLASHES);
 		}
 		$_POST[$name] = $value;
 	}
+
 	return true;
 }
 
@@ -84,11 +111,12 @@ if (isset($_POST['submit'])) {
 			</div>
 			<div>
 				<label for="phoneNumber">Your Phone Number*:</label><br />
-				<input type="tel" name="phoneNumber" required />
+				<input type="tel" name="phoneNumber" required pattern="\(\d{3}\) \d{3}-\d{4}" title="Phone number must be in the format: (xxx) xxx-xxxx" />
 			</div>
 			<div><input type="submit" name="submit" value="Contact Me" /></div>
 		</form>
 	</main>
+	<p><b><?php echo get_users(); ?></b> users have visited this site.</p>
 </body>
 
 </html>
